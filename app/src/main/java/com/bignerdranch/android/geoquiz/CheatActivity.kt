a9695db147b454e441794d2e28a35cefff5aab31
vladislawovk <vladislawovk@gmail.com>
2023-04-23 20:37:19 +0400

    Update CheatActivity to use ViewBinding

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -2,10 +2,14 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.geoquiz.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cheat)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
